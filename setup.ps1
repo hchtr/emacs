9941208e57c5b24e06fed6cc64c00e57a2aac5a2
hchtr <155396759+hchtr@users.noreply.github.com>
2025-12-03 20:52:05 +0100

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -1,1 +1,7 @@
-New-Item -ItemType SymbolicLink -Path "$env:APPDATA\.emacs.d\init.el" -Target "C:\hchtr\repos\emacs\init.el"
+$repoInitFile = "C:\hchtr\repos\emacs\init.el"
+
+$initFile = "$env:APPDATA\.emacs.d\init.el"
+
+Remove-Item $initFile -Force -ErrorAction Ignore
+
+New-Item -ItemType SymbolicLink -Path $initFile -Target $repoInitFile
